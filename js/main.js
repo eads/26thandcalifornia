744210821ce049384b23b2d1bd3af3e09bf195a7
David Eads <davideads@gmail.com>
2013-01-21 00:04:22 -0600

    messing around

--- js/main.js
+++ js/main.js
@@ -6,14 +6,27 @@ var INMATE_URL = 'http://cookcountyjail.recoveredfactory.net/api/1.0/countyinmat
 // RequireJS aliases
 require.config({
     paths: {
-        jquery: '../lib/jquery-1.8.3.min',
-        underscore: '../lib/underscore-1.4.2.min',
-        backbone: '../lib/backbone-0.9.2.min',
-        text: '../lib/text',
-        moment: '../lib/moment',
-        templates: '../templates'
+        'jquery': '../lib/jquery-1.8.3.min',
+        'underscore': '../lib/underscore-1.4.2.min',
+        'backbone': '../lib/backbone-0.9.2.min',
+        'text': '../lib/text',
+        'moment': '../lib/moment',
+        'templates': '../templates',
+        'slickgrid.core': '../lib/slickgrid/slick.core',
+        'slickgrid.grid': '../lib/slickgrid/slick.grid',
+        'slickgrid.dataview': '../lib/slickgrid/slick.dataview',
+        'slickgrid.plugins.cellrangeselector': '../lib/slickgrid/plugins/slick.cellrangeselector',
+        'slickgrid.plugins.cellrangedecorator': '../lib/slickgrid/plugins/slick.cellrangedecorator',
+        'slickgrid.plugins.cellselectionmodel': '../lib/slickgrid/plugins/slick.cellselectionmodel',
+        'jquery.event.drag': '../lib/slickgrid/lib/jquery.event.drag-2.0.min',
+        'jquery.ui': '../lib/slickgrid/lib/jquery-ui-1.8.16.custom.min',
+    },
+    shim: {
+      'slickgrid.grid': {
+        deps: ['jquery.ui', 'jquery.event.drag', 'slickgrid.core', 'slickgrid.plugins.cellrangedecorator', 'slickgrid.plugins.cellrangeselector', 'slickgrid.plugins.cellselectionmodel'],
+        exports: 'Slick',
+      }
     }
-
 });
 
 // Load our application by requiring it, then calling it's
--- js/views/InmateTableView.js
+++ js/views/InmateTableView.js
@@ -3,18 +3,40 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'slickgrid.grid',
 
     // Our apps
     'collections/InmateCollection',
 
     // Templates
     'text!templates/inmate_table.html'
-], function($, _, Backbone, InmateCollection, inmate_table) {
+], function($, _, Backbone, Slick, InmateCollection, inmate_table) {
 
     var InmateTableView = Backbone.View.extend({
         collection: InmateCollection,
         el: '#content',
         initialize: function(options) {
+            this.container = $('<div id="inmate-table">').appendTo(this.$el);
+            var columns = [
+                {'id': 'jail_id', 'name': 'jail_id', 'field': 'jail_id'},
+                {'id': 'booking_date', 'name': 'booking_date', 'field': 'booking_date'},
+                {'id': 'age_at_booking', 'name': 'age_at_booking', 'field': 'age_at_booking', 'sortable': true},
+                {'id': 'bail_amount', 'name': 'bail_amount', 'field': 'bail_amount', 'sortable': true},
+                {'id': 'charges', 'name': 'charges', 'field': 'charges'},
+                {'id': 'charges_citation', 'name': 'charges_citation', 'field': 'charges_citation'},
+                {'id': 'gender', 'name': 'gender', 'field': 'gender'},
+                {'id': 'race', 'name': 'race', 'field': 'race'},
+                {'id': 'housing_location', 'name': 'housing_location', 'field': 'housing_location'},
+                {'id': 'stay_length', 'name': 'stay_length', 'field': 'stay_length'},
+                {'id': 'discharge_date_earliest', 'name': 'discharge_date_earliest', 'field': 'discharge_date_earliest'},
+            ];
+            this.grid = new Slick.Grid('#inmate-table', [], columns, {
+                enableCellNavigation: true,
+                forceFitColumns: true,
+                enableColumnReorder: false,
+            });
+            this.grid.setSelectionModel(new Slick.CellSelectionModel());
+
             // Call 'spin' when collection AJAX request starts.
             this.collection.bind('fetch', this.spin, this);
 
@@ -23,15 +45,15 @@ define([
         },
         spin: function() {
             // Clear element and start spinner on collection start
-            this.$el.html('');
             this.spinner = new Spinner().spin(this.el);
             return this;
         },
         render: function(options) {
             // Render template and stop spinner.
-            var compiled_template = _.template(inmate_table, { inmates: this.collection.toJSON() });
-            this.$el.html(compiled_template);
             this.spinner.stop();
+            this.grid.setData(this.collection.toJSON());
+            this.grid.updateRowCount();
+            this.grid.render();
             return this;
         }
     });
